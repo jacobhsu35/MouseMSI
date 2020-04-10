6d93b692fde23feb0f63ab4607c90e294f312e6e
Yi Hsiao <hsiaoyi0504@gmail.com>
2020-04-10 09:35:11 +0800

    update location of reference file

--- two_CML_cases.sh
+++ two_CML_cases.sh
@@ -8,7 +8,7 @@ $SCRIPT_PATH/run_mutect2.sh $INPUT_FILE_DIR/TW-CML-M-016-BC_R1.fastq.bz2 \
     "@RG\tID:GP-TW-CML-M-016-BC\tSM:TW-CML-M-016-BC\tPL:illumina" \
     "@RG\tID:GP-TW-CML-M-016-D\tSM:TW-CML-M-016-D\tPL:illumina" \
     /work1/hsiaoyi0504/result/CML_hg19_2/TW-CML-M-016 \
-    /home/hsiaoyi0504/ucsc.hg19.fasta \
+    /home/hsiaoyi0504/ref/hg19/ucsc.hg19.fasta \
     /work1/hsiaoyi0504/ref/hg19/dbsnp_138.hg19.vcf
 
 $SCRIPT_PATH/run_mutect2.sh $INPUT_FILE_DIR/TW-CML-M-018-BC_R1.fastq.bz2 \
@@ -18,5 +18,5 @@ $SCRIPT_PATH/run_mutect2.sh $INPUT_FILE_DIR/TW-CML-M-018-BC_R1.fastq.bz2 \
     "@RG\tID:GP-TW-CML-M-018-BC\tSM:TW-CML-M-018-BC\tPL:illumina" \
     "@RG\tID:GP-TW-CML-M-018-D\tSM:TW-CML-M-018-D\tPL:illumina" \
     /work1/hsiaoyi0504/result/CML_hg19_2/TW-CML-M-018 \
-    /home/hsiaoyi0504/ucsc.hg19.fasta \
+    /home/hsiaoyi0504/ref/hg19/ucsc.hg19.fasta \
     /work1/hsiaoyi0504/ref/hg19/dbsnp_138.hg19.vcf
