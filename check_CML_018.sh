6265749cfe323dec5d35ee35b6d0de10c5e79245
Yi Hsiao <hsiaoyi0504@gmail.com>
2020-04-21 09:39:42 +0800

    switch to use HumanG1Kv37_decoy as reference sequence

--- check_CML_018.sh
+++ check_CML_018.sh
@@ -1,37 +1,26 @@
-VCF_GZ_FILE=/work1/hsiaoyi0504/result/CML_hg19_2/TW-CML-M-018/Mutect2.vcf.gz
+OUTPUT_DIR=/work1/hsiaoyi0504/result/CML_hg19/TW-CML-M-018
+VCF_GZ_FILE=$OUTPUT_DIR/Mutect2.vcf.gz
 BCF_TOOLS=/pkg/biology/BCFtools/BCFtools_v1.10.2/bin/bcftools
+ANNOVAR_PATH=/pkg/biology/ANNOVAR/ANNOVAR_20191024
 
-zcat $VCF_GZ_FILE | grep -P "chr3\t12632420"
-zcat $VCF_GZ_FILE | grep -P "chr3\t47037005"
-zcat $VCF_GZ_FILE | grep -P "chr3\t49896871"
-zcat $VCF_GZ_FILE | grep -P "chr3\t52255972"
-zcat $VCF_GZ_FILE | grep -P "chr5\t94852706"
-zcat $VCF_GZ_FILE | grep -P "chr5\t180219340"
-zcat $VCF_GZ_FILE | grep -P "chr7\t4050672"
-zcat $VCF_GZ_FILE | grep -P "chr9\t96070669"
-zcat $VCF_GZ_FILE | grep -P "chr10\t124392747"
-zcat $VCF_GZ_FILE | grep -P "chr13\t110437498"
-zcat $VCF_GZ_FILE | grep -P "chr15\t96877834"
-zcat $VCF_GZ_FILE | grep -P "chr17\t7578265"
-zcat $VCF_GZ_FILE | grep -P "chr17\t20109120"
-zcat $VCF_GZ_FILE | grep -P "chr19\t16513262"
-zcat $VCF_GZ_FILE | grep -P "chr19\t46511501"
-zcat $VCF_GZ_FILE | grep -P "chr20\t18462374"
+$ANNOVAR_PATH/convert2annovar.pl -format vcf4old $VCF_GZ_FILE > $OUTPUT_DIR/Mutect2.annovar 2> /dev/null
 
-# check location of gene
-# $BCF_TOOLS view --no-header -r chr3:12625100-12705725 $VCF_GZ_FILE # RAF1 gene, https://www.genecards.org/cgi-bin/carddisp.pl?gene=RAF1
-# $BCF_TOOLS view --no-header -r chr3:47021173-47051193 $VCF_GZ_FILE # NBEAL2 
-# $BCF_TOOLS view --no-header -r chr3:49895421-49907655 $VCF_GZ_FILE # CAMKV
-# $BCF_TOOLS view --no-header -r chr3:52255096-52273183 $VCF_GZ_FILE # TLR9
-# $BCF_TOOLS view --no-header -r chr5:94799599-94890711 $VCF_GZ_FILE # TTC37
-# $BCF_TOOLS view --no-header -r chr5:180217541-180242652 $VCF_GZ_FILE # MGAT1
-# $BCF_TOOLS view --no-header -r chr7:3341080-4308632 $VCF_GZ_FILE # SDK1
-# $BCF_TOOLS view --no-header -r chr9:95947198-96082854  $VCF_GZ_FILE # WNK2
-# $BCF_TOOLS view --no-header -r chr10:124320181-124403252 $VCF_GZ_FILE # DMBT1
-# $BCF_TOOLS view --no-header -r chr13:110406184-110438915 $VCF_GZ_FILE # IRS2
-# $BCF_TOOLS view --no-header -r chr15:96869157-96883492 $VCF_GZ_FILE # NR2F2
-# $BCF_TOOLS view --no-header -r chr17:7565097-7590863 $VCF_GZ_FILE # TP53
-# $BCF_TOOLS view --no-header -r chr17:19912657-20222339 $VCF_GZ_FILE # SPECC1
-# $BCF_TOOLS view --no-header -r chr19:16466050-16582896 $VCF_GZ_FILE # EPS15L1
-# $BCF_TOOLS view --no-header -r chr19:46498339-46524576 $VCF_GZ_FILE # CCDC61
-# $BCF_TOOLS view --no-header -r chr20:18447771-18465287 $VCF_GZ_FILE # POLR3F
+# check location of variant
+
+cat $OUTPUT_DIR/Mutect2.annovar | grep -P "3\t12632420"
+cat $OUTPUT_DIR/Mutect2.annovar | grep -P "3\t47037005"
+cat $OUTPUT_DIR/Mutect2.annovar | grep -P "3\t49896871"
+cat $OUTPUT_DIR/Mutect2.annovar | grep -P "3\t52255972"
+cat $OUTPUT_DIR/Mutect2.annovar | grep -P "5\t94852706"
+cat $OUTPUT_DIR/Mutect2.annovar | grep -P "5\t180219340"
+cat $OUTPUT_DIR/Mutect2.annovar | grep -P "7\t4050672"
+cat $OUTPUT_DIR/Mutect2.annovar | grep -P "9\t96070669"
+cat $OUTPUT_DIR/Mutect2.annovar | grep -P "10\t124392747"
+cat $OUTPUT_DIR/Mutect2.annovar | grep -P "13\t110437498"
+cat $OUTPUT_DIR/Mutect2.annovar | grep -P "15\t96877834"
+cat $OUTPUT_DIR/Mutect2.annovar | grep -P "17\t7578265"
+cat $OUTPUT_DIR/Mutect2.annovar | grep -P "17\t20109120"
+cat $OUTPUT_DIR/Mutect2.annovar | grep -P "19\t16513262"
+cat $OUTPUT_DIR/Mutect2.annovar | grep -P "19\t46511501"
+cat $OUTPUT_DIR/Mutect2.annovar | grep -P "20\t18462374"
+
--- two_CML_cases.sh
+++ two_CML_cases.sh
@@ -7,9 +7,11 @@ $SCRIPT_PATH/run_mutect2.sh $INPUT_FILE_DIR/TW-CML-M-016-BC_R1.fastq.bz2 \
     $INPUT_FILE_DIR/TW-CML-M-016-D_R2.fastq.bz2 \
     "@RG\tID:GP-TW-CML-M-016-BC\tSM:TW-CML-M-016-BC\tPL:illumina" \
     "@RG\tID:GP-TW-CML-M-016-D\tSM:TW-CML-M-016-D\tPL:illumina" \
-    /work1/hsiaoyi0504/result/CML_hg19_2/TW-CML-M-016 \
-    /home/hsiaoyi0504/ref/hg19/ucsc.hg19.fasta \
-    /work1/hsiaoyi0504/ref/hg19/dbsnp_138.hg19.vcf
+    /work1/hsiaoyi0504/result/CML_hg19/TW-CML-M-016 \
+    /work2/u00srx00/reference/GATK/human_g1k_v37/human_g1k_v37_decoy.fasta \
+    /project/GP1/u3710062/AI_SHARE/reference/GATK_bundle/2.8/b37/dbsnp_138.b37.vcf \
+    /project/GP1/reference/Homo_sapiens/GATK/Mutect2/af-only-gnomad.raw.sites.b37.vcf.gz \
+    /project/GP1/reference/Homo_sapiens/GATK/Mutect2/GetPileupSummaries/small_exac_common_3_b37.vcf.gz
 
 $SCRIPT_PATH/run_mutect2.sh $INPUT_FILE_DIR/TW-CML-M-018-BC_R1.fastq.bz2 \
     $INPUT_FILE_DIR/TW-CML-M-018-BC_R2.fastq.bz2 \
@@ -17,6 +19,8 @@ $SCRIPT_PATH/run_mutect2.sh $INPUT_FILE_DIR/TW-CML-M-018-BC_R1.fastq.bz2 \
     $INPUT_FILE_DIR/TW-CML-M-018-D_R2.fastq.bz2 \
     "@RG\tID:GP-TW-CML-M-018-BC\tSM:TW-CML-M-018-BC\tPL:illumina" \
     "@RG\tID:GP-TW-CML-M-018-D\tSM:TW-CML-M-018-D\tPL:illumina" \
-    /work1/hsiaoyi0504/result/CML_hg19_2/TW-CML-M-018 \
-    /home/hsiaoyi0504/ref/hg19/ucsc.hg19.fasta \
-    /work1/hsiaoyi0504/ref/hg19/dbsnp_138.hg19.vcf
+    /work1/hsiaoyi0504/result/CML_hg19/TW-CML-M-018 \
+    /work2/u00srx00/reference/GATK/human_g1k_v37/human_g1k_v37_decoy.fasta \
+    /project/GP1/u3710062/AI_SHARE/reference/GATK_bundle/2.8/b37/dbsnp_138.b37.vcf \
+    /project/GP1/reference/Homo_sapiens/GATK/Mutect2/af-only-gnomad.raw.sites.b37.vcf.gz \
+    /project/GP1/reference/Homo_sapiens/GATK/Mutect2/GetPileupSummaries/small_exac_common_3_b37.vcf.gz
