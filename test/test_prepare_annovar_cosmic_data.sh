27e2d23f0fde3775a48882672d52e62990bc577f
Yi Hsiao <hsiaoyi0504@gmail.com>
2020-06-07 22:28:33 +0800

    use normalized version of vcf to generate txt files for Annovar annotation

--- test/test_prepare_annovar_cosmic_data.sh
+++ test/test_prepare_annovar_cosmic_data.sh
@@ -1,5 +1,5 @@
 ANNOVAR_PATH=/pkg/biology/ANNOVAR/ANNOVAR_20191127
 DATA_PATH=/project/GP1/j116831/AI_Labs/data/COSMIC
 
-./prepare_annovar_user.pl -dbtype cosmic $DATA_PATH/CosmicMutantExport_GRCh37_v91.tsv -vcf $DATA_PATH/CosmicCodingMuts_GRCh37_v91.vcf > $DATA_PATH/cosmic_coding_GRCh37_v91.txt
-./prepare_annovar_user.pl -dbtype cosmic $DATA_PATH/CosmicNCV_GRCh37_v91.tsv -vcf $DATA_PATH/CosmicNonCodingVariants_GRCh37_v91.vcf > $DATA_PATH/cosmic_noncoding_GRCh37_v91.txt
+./prepare_annovar_user.pl -dbtype cosmic $DATA_PATH/CosmicMutantExport_GRCh37_v91.tsv -vcf $DATA_PATH/CosmicCodingMuts_GRCh37_v91.normal.vcf > $DATA_PATH/cosmic_coding_GRCh37_v91.txt
+./prepare_annovar_user.pl -dbtype cosmic $DATA_PATH/CosmicNCV_GRCh37_v91.tsv -vcf $DATA_PATH/CosmicNonCodingVariants_GRCh37_v91.normal.vcf > $DATA_PATH/cosmic_noncoding_GRCh37_v91.txt
