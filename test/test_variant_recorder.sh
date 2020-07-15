ab8f9252fdff7ad677bc362dc2a2b8db745bb547
Yi Hsiao <hsiaoyi0504@gmail.com>
2020-07-15 13:48:04 +0800

    fix output suffix of test_variant_recorder.sh

--- test/test_variant_recorder.sh
+++ test/test_variant_recorder.sh
@@ -3,4 +3,4 @@ export PATH=$HTSLIB_PATH:$PATH
 
 module load biology/Perl/default
 
-/pkg/biology/Ensembl-VEP/Ensembl-VEP_v99.2/variant_recoder -i ./test_data/test2AMLvariants.vcf --grch37 --pretty > ./test_data_output/test2AMLvariants.variant_recoder.vcf
+/pkg/biology/Ensembl-VEP/Ensembl-VEP_v99.2/variant_recoder -i ./test_data/test2AMLvariants.vcf --grch37 --pretty > ./test_data_output/test2AMLvariants.variant_recoder.json
